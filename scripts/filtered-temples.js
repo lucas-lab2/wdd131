68f80a7e60b8f13162fb165f2ce27f6bc1e24407
Lucas Felipe <lucas.miranda880@outlook.com>
2025-05-31 15:35:32 -0300

    version 1 of filtered-temples project

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -7,70 +7,70 @@ const temples = [
     location: "Kirtland, Ohio, USA",
     dedicatedDate: "1836-03-27",
     area: 11186,
-    imageUrl: "https://www.churchofjesuschrist.org/.../kirtland-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/kirtland-temple/kirtland-temple-1275-main.jpg"
   },
   {
     name: "St. George Temple",
     location: "St. George, Utah, USA",
     dedicatedDate: "1877-04-06",
     area: 18633,
-    imageUrl: "https://www.churchofjesuschrist.org/.../st-george-temple.jpg"
+    imageUrl: "https://www.churchofjesuschrist.org/imgs/5513d22a19c811eebaf2eeeeac1e7e438422c51d/full/800%2C/0/default"
   },
   {
     name: "Salt Lake Temple",
     location: "Salt Lake City, Utah, USA",
     dedicatedDate: "1893-04-06",
     area: 253000,
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/9/93/Salt_Lake_Temple%2C_Utah_-_Sept_2004-2.jpg"
+    imageUrl: "https://www.churchofjesuschrist.org/imgs/92c33bcbf9cf85483e008d6871f8ced5f6d7b661/full/800%2C/0/default"
   },
   {
     name: "Laie Hawaii Temple",
     location: "Laie, Hawaii, USA",
     dedicatedDate: "1919-11-27",
     area: 72000,
-    imageUrl: "https://www.churchofjesuschrist.org/.../laie-hawaii-temple.jpg"
+    imageUrl: "https://www.churchofjesuschrist.org/imgs/809f567ccf240d2f1c8e457e8c81fbd94ef96759/full/800%2C/0/default"
   },
   {
     name: "Los Angeles Temple",
     location: "Los Angeles, California, USA",
     dedicatedDate: "1956-03-11",
     area: 79000,
-    imageUrl: "https://www.churchofjesuschrist.org/.../la-temple.jpg"
+    imageUrl: "https://www.ldstemple.pics/wp-content/uploads/los-angeles-temple-glory-to-the-highest.jpg"
   },
   {
     name: "Washington D.C. Temple",
     location: "Kensington, Maryland, USA",
     dedicatedDate: "1974-11-19",
     area: 160000,
-    imageUrl: "https://www.churchofjesuschrist.org/.../dc-temple.jpg"
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1P0emotD07_SDXo7awdLVPkNW-jmSJHcu8g&s"
   },
   {
     name: "London England Temple",
     location: "Newchapel, Surrey, England",
     dedicatedDate: "1958-09-07",
     area: 21000,
-    imageUrl: "https://www.churchofjesuschrist.org/.../london-temple.jpg"
+    imageUrl: "https://www.churchofjesuschrist.org/imgs/914ec5701c2435b4cfa50668de85df2aebf68f26/full/800%2C/0/default"
   },
   {
     name: "Aba Nigeria Temple",
     location: "Aba, Nigeria",
     dedicatedDate: "2005-08-07",
     area: 11500,
-    imageUrl: "https://www.churchofjesuschrist.org/.../aba-temple.jpg"
+    imageUrl: "https://www.churchofjesuschrist.org/imgs/7210c09be95c4474772ae52e5f31c23c08112784/full/800%2C/0/default"
   },
   {
     name: "Accra Ghana Temple",
     location: "Accra, Ghana",
     dedicatedDate: "2004-01-11",
     area: 83000,
-    imageUrl: "https://www.churchofjesuschrist.org/.../accra-temple.jpg"
+    imageUrl: "https://www.churchofjesuschrist.org/imgs/7cf8e8b9e5a5a1f379d4e2c9bc2166f9c6007aca/full/800%2C/0/default"
   },
   {
     name: "Apia Samoa Temple",
     location: "Apia, Samoa",
     dedicatedDate: "1983-09-18",
     area: 6300,
-    imageUrl: "https://www.churchofjesuschrist.org/.../apia-temple.jpg"
+    imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/apia-samoa-temple/apia-samoa-temple-13905-main.jpg"
   }
 ];
 
